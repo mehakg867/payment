0550079161ccc87230244353cd3a109e5767fbc7
Mehak goyal <47946110+mehakg867@users.noreply.github.com>
2022-10-01 12:48:43 +0530

    app

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
-import AppRoute from "./Route/AppRoute";
+import AppRoute from './Route/AppRoute';
 
 
 function App() {
